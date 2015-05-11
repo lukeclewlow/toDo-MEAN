96de3f43006df2ba9f4a3409b6d0166c52388b59
Luke Clewlow <luke.clewlow@gmail.com>
2015-05-11 18:30:27 +0100

    updated typo in app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@
 
 	//view engine setup
 
-	app.set('views', path.join(__dirname, views));
+	app.set('views', path.join(__dirname, 'views'));
 	app.engine('html', require('ejs').renderFile);
 	app.set('view engine', 'html');
 
